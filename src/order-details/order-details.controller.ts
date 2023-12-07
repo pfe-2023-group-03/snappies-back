7fc6d561a2cc74ff1657fe66645a5b1b48b0eba0
Lucas <lucas.fuentes@student.vinci.be>
2023-12-07 10:49:12 +0100

    feat: orderDetail controller

--- src/order-details/order-details.controller.ts
+++ src/order-details/order-details.controller.ts
@@ -1,4 +1,53 @@
 import { Controller } from '@nestjs/common';
+import { OrderDetailsService } from './order-details.service';
+import { CreateOrderDetailDto } from './dto/create-orderDetail.dto';
+import { UpdateOrderDetailDto } from './dto/update-orderDetail.dto';
 
 @Controller('order-details')
-export class OrderDetailsController {}
+export class OrderDetailsController {
+
+    constructor(private orderDetailsService: OrderDetailsService){}
+
+
+    // get all orderDetails
+    findAll() {
+        return this.orderDetailsService.findAll();
+    }
+
+    // get one orderDetail by id
+    findOne(orderId: number, articleId: number) {
+        return this.orderDetailsService.findOne(orderId, articleId);
+    }
+
+    // add orderDetail to order
+    create( createOrderDetailDto : CreateOrderDetailDto ) {
+        return this.orderDetailsService.create(createOrderDetailDto);
+    }
+
+    // update orderDetail
+    update(orderId: number, articleId: number, updateOrderDetailDto : UpdateOrderDetailDto) {
+        return this.orderDetailsService.update(orderId, articleId, updateOrderDetailDto);
+    }
+
+    // delete orderDetail
+    remove(orderId: number, articleId: number) {
+        return this.orderDetailsService.remove(orderId, articleId);
+    }
+
+    // get all orderDetails for an order
+    findByOrder(orderId: number) {
+        return this.orderDetailsService.findByOrder(orderId);
+    }
+
+    // get quantity of an article in an order
+    getQuantityOfArticleOrder(orderId: number, articleId: number) {
+        return this.orderDetailsService.getQuantityOfArticleOrder(orderId, articleId);
+    }
+
+    // get sum of all quantities of articles in an order
+    getSumQuantityOrder(orderId: number) {
+        return this.orderDetailsService.getSumQuantityOrder(orderId);
+    }
+
+
+}
