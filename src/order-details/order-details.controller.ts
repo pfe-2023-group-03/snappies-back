b444afc4df53af2a5cc8e900b7de8745968e65b9
Lucas <lucas.fuentes@student.vinci.be>
2023-12-07 10:52:52 +0100

    feat: add permission

--- src/order-details/order-details.controller.ts
+++ src/order-details/order-details.controller.ts
@@ -2,6 +2,8 @@ import { Controller } from '@nestjs/common';
 import { OrderDetailsService } from './order-details.service';
 import { CreateOrderDetailDto } from './dto/create-orderDetail.dto';
 import { UpdateOrderDetailDto } from './dto/update-orderDetail.dto';
+import { Roles } from 'src/decorators/role.decorator';
+import { Role } from 'src/enums/role.enum';
 
 @Controller('order-details')
 export class OrderDetailsController {
@@ -10,26 +12,31 @@ export class OrderDetailsController {
 
 
     // get all orderDetails
+    @Roles(Role.Deliverer,Role.Admin)
     findAll() {
         return this.orderDetailsService.findAll();
     }
 
     // get one orderDetail by id
+    @Roles(Role.Deliverer,Role.Admin)
     findOne(orderId: number, articleId: number) {
         return this.orderDetailsService.findOne(orderId, articleId);
     }
 
     // add orderDetail to order
+    @Roles(Role.Deliverer,Role.Admin)
     create( createOrderDetailDto : CreateOrderDetailDto ) {
         return this.orderDetailsService.create(createOrderDetailDto);
     }
 
     // update orderDetail
+    @Roles(Role.Deliverer,Role.Admin)
     update(orderId: number, articleId: number, updateOrderDetailDto : UpdateOrderDetailDto) {
         return this.orderDetailsService.update(orderId, articleId, updateOrderDetailDto);
     }
 
     // delete orderDetail
+    @Roles(Role.Admin)
     remove(orderId: number, articleId: number) {
         return this.orderDetailsService.remove(orderId, articleId);
     }
@@ -40,11 +47,13 @@ export class OrderDetailsController {
     }
 
     // get quantity of an article in an order
+    @Roles(Role.Deliverer,Role.Admin)
     getQuantityOfArticleOrder(orderId: number, articleId: number) {
         return this.orderDetailsService.getQuantityOfArticleOrder(orderId, articleId);
     }
 
     // get sum of all quantities of articles in an order
+    @Roles(Role.Deliverer,Role.Admin)
     getSumQuantityOrder(orderId: number) {
         return this.orderDetailsService.getSumQuantityOrder(orderId);
     }
