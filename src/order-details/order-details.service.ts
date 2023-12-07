a5178efd1ed81009b5dc1613f2892d4d63489073
Lucas <lucas.fuentes@student.vinci.be>
2023-12-07 10:42:06 +0100

    feat: orderDetails service

--- src/order-details/order-details.service.ts
+++ src/order-details/order-details.service.ts
@@ -2,32 +2,64 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OrderDetail } from './entites/orderDetail.entity';
 import { Repository } from 'typeorm';
+import { CreateOrderDetailDto } from './dto/create-orderDetail.dto';
+import { UpdateOrderDetailDto } from './dto/update-orderDetail.dto';
 
 @Injectable()
 export class OrderDetailsService {
 
     constructor(
         @InjectRepository(OrderDetail)
-        private articleRepository : Repository<OrderDetail>
+        private orderDetailRepository : Repository<OrderDetail>
     ) {}
 
     // get all orderDetails
     findAll() {
-        return this.articleRepository.find();
+        return this.orderDetailRepository.find();
     }
 
     // get one orderDetail by id
     findOne(orderId: number, articleId: number) {
-        return this.articleRepository.findOne({
+        return this.orderDetailRepository.findOne({
             where: { orderId, articleId }
         });
     }
 
     // add orderDetail to order
-    create(: ) {
-        return this.articleRepository.save(orderDetail);
+    create( createOrderDetailDto: CreateOrderDetailDto ) {
+        return this.orderDetailRepository.save(createOrderDetailDto);
     }
 
+    // update orderDetail
+    update(orderId: number, articleId: number, updateOrderDetailDto: UpdateOrderDetailDto) {
+        return this.orderDetailRepository.update({ orderId, articleId }, updateOrderDetailDto);
+    }
 
+    // delete orderDetail
+    remove(orderId: number, articleId: number) {
+        return this.orderDetailRepository.delete({ orderId, articleId });
+    }
 
+    // get all orderDetails for an order
+    findByOrder(orderId: number) {
+        return this.orderDetailRepository.find({
+            where: { orderId }
+        });
+    }
+
+    // get quantity of an article in an order
+    getQuantityOfArticleOrder(orderId: number, articleId: number) {
+        return this.orderDetailRepository.findOne({
+            select: ['quantity'],
+            where: { orderId, articleId }
+        });
+    }
+
+    // get sum of all quantities of articles in an order
+    getSumQuantityOrder(orderId: number) {
+        return this.orderDetailRepository.createQueryBuilder('orderDetail')
+            .select('SUM(orderDetail.quantity)', 'sum')
+            .where('orderDetail.orderId = :orderId', { orderId })
+            .getRawOne();
+    }
 }
