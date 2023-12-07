bafb1e774ef77807fd3f9fb85ee3ea551feeedc0
Lucas <lucas.fuentes@student.vinci.be>
2023-12-07 09:44:23 +0100

    refactor: fk order-client

--- src/client/entities/client.entity.ts
+++ src/client/entities/client.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Order } from "src/orders/entities/order.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Client {
@@ -17,4 +18,7 @@ export class Client {
 
     @Column({nullable: false})
     phone: string;
+
+    @OneToMany(() => Order, order => order.client)
+    orders: Order[];
 }
