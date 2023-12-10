b662020e8c73a8ccd9b8b500f4a31e517341bb32
Nitroc <cdhaeyere@proton.me>
2023-12-10 15:50:21 +0100

    feat: input validation

--- src/deliveries/dto/create-delivery.dto.ts
+++ src/deliveries/dto/create-delivery.dto.ts
@@ -1,15 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsIn, IsNotEmpty, IsNumber, IsString } from "class-validator";
 import { DeliveryState } from "src/enums/deliveryState.enum";
 
 export class CreateDeliveryDto {
     
     @ApiProperty()
+    @IsIn([DeliveryState.Default, DeliveryState.Delivery, DeliveryState.Finished, DeliveryState.Preparation])
     state: DeliveryState;
     
     @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
     title: string;
 
     @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
     userId: number;
     
 }
--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,14 +1,20 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsIn, IsNotEmpty, IsNumber } from "class-validator";
 import { orderState } from "src/enums/orderState.enum";
 
 export class CreateOrderDto {
 
     @ApiProperty()
+    @IsIn([orderState.Delivery, orderState.Done, orderState.Surplus])
     state: orderState;
 
     @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
     clientId: number;
 
     @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
     deliveryId: number;
 }
